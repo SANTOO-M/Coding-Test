957307dc0bc623a5e18c1534ed35d7ffb0297123
SANTHOSHKUMAR M <105714799+SANTOO-M@users.noreply.github.com>
2022-12-19 18:53:52 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -4,20 +4,20 @@ class Anagram{
 	public static void main(String args[]){
 	String s1="Binary";	
 	String s2="Brainy";
-	char c1[]=s1.toCharArray();
-	char c2[]=s2.toCharArray();
+	char c1[]=s1.toCharArray();	// converts String to char array
+	char c2[]=s2.toCharArray();	// converts String to char array
 	if(c1.length!=c2.length){
 	System.out.println("Not an Anagram");	
 	System.exit(0);
 	}
-	Arrays.sort(c1);
-	Arrays.sort(c2);
+	Arrays.sort(c1);		//sorting the char array
+	Arrays.sort(c2);		//sorting the char array
 	for(int i=0;i<c1.length;i++){
 		if(c1[i]!=c2[i]){
 		System.out.println("Not an Anagram");
 		System.exit(0);	
 		}
 	}
-System.out.println("Its an Anagram");	
+	System.out.println("Its an Anagram");	
 }
 }
