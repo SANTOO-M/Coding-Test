0c935ed5dd462331f4ddd454b5968b4e3d4b5fa6
SANTHOSHKUMAR M <105714799+SANTOO-M@users.noreply.github.com>
2022-12-23 23:33:58 +0530

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,3 +1,11 @@
+/*Write a Java program to print the following grid.
+Expected Output:
+____________________________
+____________________________
+____________________________
+____________________________
+____________________________
+*/
 public class Grid{
 	public static void main(String args[]){
    	for(int i=1;i<10;i++){
