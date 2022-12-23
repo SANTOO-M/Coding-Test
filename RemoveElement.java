cd2ac8711a775c46df17aee2f153bd951ad9cbcc
SANTHOSHKUMAR M <105714799+SANTOO-M@users.noreply.github.com>
2022-12-23 23:38:49 +0530

    Update RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,3 +1,6 @@
+/*Write a java program to remove a specific number: 23 from the following array:
+arr[11,22,33,34,43,32, 23]*/
+
 import java.util.Arrays;
 public class RemoveElement{
     public static void main(String args[]){
