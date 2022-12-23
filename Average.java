2b2e0338af463c73aaea8dfdfcab216a66f067bb
SANTHOSHKUMAR M <105714799+SANTOO-M@users.noreply.github.com>
2022-12-23 23:30:08 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -1,3 +1,6 @@
+/*Write a Java program that takes three numbers as input to calculate and
+print the average of the numbers*/
+
 import java.util.Scanner;
 public class Average{
    public static void main(String args[]){
